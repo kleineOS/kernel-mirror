15d46bda8bda2dde9695e6f94d60258f6c3fa5c3
piguycs <piguycs@pm.me>
2026-09-08 19:02:02 +0200

    pretty print the trap frame

--- kernel/src/trap.rs
+++ kernel/src/trap.rs
@@ -14,11 +14,14 @@ pub struct TrapFrame {
     pub sp: usize,
     pub gp: usize,
     pub tp: usize,
+
     pub t0: usize,
     pub t1: usize,
     pub t2: usize,
+
     pub s0: usize,
     pub s1: usize,
+
     pub a0: usize,
     pub a1: usize,
     pub a2: usize,
@@ -27,6 +30,7 @@ pub struct TrapFrame {
     pub a5: usize,
     pub a6: usize,
     pub a7: usize,
+
     pub s2: usize,
     pub s3: usize,
     pub s4: usize,
@@ -37,10 +41,28 @@ pub struct TrapFrame {
     pub s9: usize,
     pub s10: usize,
     pub s11: usize,
+
     pub t3: usize,
     pub t4: usize,
     pub t5: usize,
     pub t6: usize,
+}
+
+impl core::fmt::Display for TrapFrame {
+    #[rustfmt::skip]
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        writeln!(f)?;
+        writeln!(f, "t0={:#018x}  a0={:#018x}  s0={:#018x}   s8={:#018x}", self.t0, self.a0, self.s0, self.s8)?;
+        writeln!(f, "t1={:#018x}  a1={:#018x}  s1={:#018x}   s9={:#018x}", self.t1, self.a1, self.s1, self.s9)?;
+        writeln!(f, "t2={:#018x}  a2={:#018x}  s2={:#018x}  s10={:#018x}", self.t2, self.a2, self.s2, self.s10)?;
+        writeln!(f, "t3={:#018x}  a3={:#018x}  s3={:#018x}  s11={:#018x}", self.t3, self.a3, self.s3, self.s11)?;
+        writeln!(f, "t4={:#018x}  a4={:#018x}  s4={:#018x}   ra={:#018x}", self.t4, self.a4, self.s4, self.ra)?;
+        writeln!(f, "t5={:#018x}  a5={:#018x}  s5={:#018x}   sp={:#018x}", self.t5, self.a5, self.s5, self.sp)?;
+        writeln!(f, "t6={:#018x}  a6={:#018x}  s6={:#018x}   gp={:#018x}", self.t6, self.a6, self.s6, self.gp)?;
+        writeln!(f, "tp={:#018x}  a7={:#018x}  s7={:#018x}",              self.tp, self.a7, self.s7)?;
+
+        Ok(())
+    }
 }
 
 #[unsafe(no_mangle)]
@@ -67,7 +89,7 @@ extern "C" fn kerneltrap(frame: &mut TrapFrame) {
     let cause = scause & ((1 << 63) - 1);
 
     todo!(
-        "Kernel trap is not yet implemented. int={} cause={} sepc={sepc:#x?} stval={stval:#x?} {:#x?}",
+        "Kernel trap is not yet implemented. int={} cause={} sepc={sepc:#x?} stval={stval:#x?} {}",
         int,
         cause,
         frame
