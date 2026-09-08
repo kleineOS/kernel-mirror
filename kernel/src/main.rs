8ec19f115abcd6649d999ec958c6600cd45b7da8
piguycs <piguycs@pm.me>
2026-09-08 22:11:03 +0200

    the bss section is cleared properly now

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -50,8 +50,8 @@ unsafe fn clear_bss() {
         static mut __bss_end: usize;
     }
 
-    let start = &raw mut __bss_start;
-    let end = &raw mut __bss_end;
+    let start = &raw mut __bss_start as *mut u8;
+    let end = &raw mut __bss_end as *mut u8;
 
     let len = end as usize - start as usize;
 
